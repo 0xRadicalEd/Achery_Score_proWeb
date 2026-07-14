33d7467a1b1b14d2db354ed62d58c35ca0637ec4
Atomsk Ed Shannon <flapjack_finance@proton.me>
2026-07-14 06:15:13 -0700

    Prepare for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Achery_Score_proWeb/',
   plugins: [react()],
   server: {
     port: 5173,
